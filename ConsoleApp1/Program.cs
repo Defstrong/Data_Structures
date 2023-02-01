597ba8acb1675b1044bf3bd429f42563f3f94a68
Defstrong <sfayzulloev1@gmail.com>
2023-02-01 15:38:26 +0300

    LinkedList delete program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Linq.Expressions;
-
-var linkedList = new CastomLinkedList<int>();
-linkedList.Add(1);
-linkedList.Add(2);
-linkedList.Add(3);
-linkedList.Add(4);
-linkedList.Add(5);
-linkedList.Remove(6);
-
-foreach (var ii in linkedList)
-    Console.WriteLine(ii);
+﻿using System.Collections;
 
 class Node<T>
 {
